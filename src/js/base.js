245243fec6df08c16970cfd80af71594f93b631f
Heyde Moura <moura.heyde@gmail.com>
2016-01-12 18:13:51 -0300

    Implementada animação de scroll no navbar

---
+++ src/js/base.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('#nav-wrapper').height($("#nav").height());
+    
+   	$(window).scroll(function() {
+   		var scroll = $(window).scrollTop();
+
+   		
+   		if ( scroll >= 0 && scroll <= 40) {
+   			$('.navbar-transparent').removeClass('navbar-transparent-noscroll');	
+   			$('.navbar-transparent').removeClass('navbar-transparent-scrollhalf');
+   			$('.navbar-transparent').addClass('navbar-transparent-noscroll');	
+   			
+   		}
+
+   		if ( scroll > 40 && scroll <= 80 ) {
+   			$('.navbar-transparent').removeClass('navbar-transparent-noscroll');	
+   			$('.navbar-transparent').removeClass('navbar-transparent-scrolled');	
+   			$('.navbar-transparent').addClass('navbar-transparent-scrollhalf');	
+   			
+   		}
+   		
+   		if ( scroll > 80 )
+   		{
+	   		$('.navbar-transparent').removeClass('navbar-transparent-scrolled');	
+	   		$('.navbar-transparent').removeClass('navbar-transparent-scrollhalf');	
+	   		$('.navbar-transparent').addClass('navbar-transparent-scrolled');
+	   		
+   		}
+   	});
+});
